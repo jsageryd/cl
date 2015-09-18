ee2a266bc0596773276df511a43f0c792a3203e8
Johan Sageryd <j@1616.se>
2015-09-18 08:55:08 +0200

    Subjects

--- cl.rb
+++ cl.rb
@@ -5,6 +5,8 @@ tags << 'HEAD'
 
 commits_with_messages = `git log --log-size --format='%H%n%b' --grep '^cl:'`
 
+$subjects = Hash[`git log --format='%H|||%s'`.split("\n").map { |l| l.split('|||') }]
+
 deindenters = []
 
 last_tag = nil
@@ -36,7 +38,7 @@ end
 
 def print_lists(lists_for_tag, commit, indent = 0)
   lists_for_tag[commit].reverse.each do |c|
-    puts "#{'  ' * indent} - #{c}"
+    puts "#{'  ' * indent} - #{$subjects[c]}"
     if lists_for_tag[c]
       print_lists(lists_for_tag, c, indent + 1)
     end
